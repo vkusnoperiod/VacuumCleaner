1e0c365a216897f1d3439003714c8b26bb0b30d0
voroninmms <msvoronin_1@edu.hse.ru>
2023-06-28 17:56:43 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <tgbot/tgbot.h>
+int main() {
+    TgBot::Bot bot("5822976994:AAHPW5pkRyOoUpqYMM-iN-HUOFj4mCvPmS4");
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Привет, я бот!");
+    });
+
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Иди нахуй еблан");
+    });
+
+    try {
+        bot.getApi().deleteWebhook();
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            bot.getApi().deleteWebhook();
+            longPoll.start();
+        }
+    } catch (const std::exception& e) {
+        std::cerr << "Ошибка: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
